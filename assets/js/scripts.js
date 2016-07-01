7a71954af61e9f06075577f545d2f7034dfe1e73
edgar chris <edgarchris4@gmail.com>
2016-07-01 17:00:24 +0300

    phone and email validation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function () {
+
+
     $('.spec').hide();
-    $('input:radio[name=capacity]').click(function(){
+    $('input:radio[name=capacity]').click(function () {
         $('.spec').toggle();
     });
     $(function () {
@@ -18,6 +20,12 @@ jQuery(document).ready(function () {
     /*
      Form
      */
+
+    function validate() {
+
+
+    }
+
     $('.registration-form fieldset:first-child').fadeIn('slow');
 
     $('.registration-form input[type="text"], .registration-form input[type="password"], .registration-form textarea').on('focus', function () {
@@ -28,14 +36,38 @@ jQuery(document).ready(function () {
     $('.registration-form .btn-next').on('click', function () {
         var parent_fieldset = $(this).parents('fieldset');
         var next_step = true;
+        var email = $('#email').val()
+        var phone = $('#phone').val()
 
-        parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function () {
+        parent_fieldset.find('input[type="text"], input[type="password"],  input[name="email"], input[type="radio"] textarea').each(function () {
             if ($(this).val() == "") {
                 $(this).addClass('input-error');
                 next_step = false;
             }
             else {
                 $(this).removeClass('input-error');
+            }
+
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var pe=/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+            if (!re.test(email)) {
+                $('#email').addClass('input-error');
+                $('.emailError').text('Enter a valid email address');
+                next_step = false;
+
+            } else {
+                $(this).removeClass('input-error');
+                $('.emailError').text('');
+            }
+
+            if (!pe.test(phone) ){
+                $('#phone').addClass('input-error');
+                $('.phoneError').text('Enter a valid  phone number');
+                next_step = false;
+
+            } else {
+                $(this).removeClass('input-error');
+                $('.phoneError').text('');
             }
         });
 
@@ -57,12 +89,13 @@ jQuery(document).ready(function () {
     // submit
     $('.registration-form').on('submit', function (e) {
 
-        $(this).find('input[type="text"], input[type="password"],input[type="email"],input[type="radio"],textarea').each(function () {
+        $(this).find('input[type="text"], input[type="password"], input[name="email"], input[type="radio"],textarea').each(function () {
             if ($(this).val() == "") {
                 e.preventDefault();
                 $(this).addClass('input-error');
             }
             else {
+
                 $(this).removeClass('input-error');
             }
         });
